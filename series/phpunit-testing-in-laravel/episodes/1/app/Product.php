5555c435c61ec3b4a37238fe3ad0a9b57b464a6e
Rizart Dokollari <r.dokollari@gmail.com>
2016-05-16 04:25:49 +0000

    Construct basic example of unit test.

---
+++ series/phpunit-testing-in-laravel/episodes/1/app/Product.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @author Rizart Dokollari <***REMOVED***>
+ * @since 5/16/16
+ */
+
+namespace App;
+
+
+class Product
+{
+    private $name;
+    private $price;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function name()
+    {
+        return $this->name;
+    }
+
+    public function setPrice($price)
+    {
+        $this->price = $price;
+    }
+
+    public function price()
+    {
+        return $this->price;
+    }
+}
---
+++ series/phpunit-testing-in-laravel/episodes/1/tests/unit/ProductTest.php
@@ -0,0 +1,47 @@
+<?php
+use App\Product;
+
+/**
+ * @author Rizart Dokollari <***REMOVED***>
+ * @since 5/16/16
+ */
+class ProductTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     * @dataProvider productDataProvider
+     * @param $product
+     */
+    public function a_product_has_name($product)
+    {
+        $product = new Product($product);
+
+        $this->assertSame($product, $product->name());
+    }
+
+    /**
+     * @test
+     * @dataProvider productDataProvider
+     * @param $productName
+     * @param $productPrice
+     */
+    public function a_product_has_price($productName, $productPrice)
+    {
+        $product = new Product($productName);
+
+        $product->setPrice($productPrice);
+
+        $this->assertSame($productPrice, $product->price());
+    }
+
+
+    public function productDataProvider()
+    {
+        return [
+            ['name' => 'Fallout 4', 'price' => 40],
+            ['name' => 'Fallout 3', 'price' => 30],
+            ['name' => 'Fallout 2', 'price' => 20],
+            ['name' => 'Fallout 1', 'price' => 10]
+        ];
+    }
+}
