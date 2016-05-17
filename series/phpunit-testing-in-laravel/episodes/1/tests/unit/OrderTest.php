e8df3d97485c50c8726b9a318018a4f041728649
Rizart Dokollari <r.dokollari@gmail.com>
2016-05-17 04:55:00 +0000

    Unit test: An order consists of products.

---
+++ series/phpunit-testing-in-laravel/episodes/1/tests/unit/OrderTest.php
@@ -0,0 +1,29 @@
+<?php
+use App\Order;
+use App\Product;
+
+/**
+ * @author Rizart Dokollari <r.dokollari@gmail.com>
+ * @since 5/17/16
+ */
+class OrderTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function an_order_consists_of_products()
+    {
+        $order = new Order;
+
+        $product = new Product('Fallout 4');
+        $product2 = new Product('Fallout 3');
+
+        $order->add($product);
+        $order->add($product2);
+
+        $this->assertCount(2, $order->products());
+
+        $product3 = new Product('Fallout 2');
+        $order->add($product3);
+
+        $this->assertCount(3, $order->products());
+    }
+}
