2bc54d7f5202dcc9ad23dabf0c2ff5ee3f1bb99d
Rizart Dokollari <r.dokollari@gmail.com>
2016-05-19 04:52:12 +0000

    Testing eloquent: Use integration tests instead of mocking stuff for unit test.

---
+++ series/phpunit-testing-in-laravel/episodes/1/tests/integration/app/ArticleTest.php
@@ -0,0 +1,25 @@
+<?php
+use App\Article;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+/**
+ * @author Rizart Dokollari <r.dokollari@gmail.com>
+ * @since 5/19/16
+ */
+class ArticleTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_fetches_trending_articles()
+    {
+        factory(Article::class, 2)->create();
+        factory(Article::class)->create(['reads' => 10]);
+        $mostPopular = factory(Article::class)->create(['reads' => 20]);
+
+        $articles = Article::trending()->get();
+
+        $this->assertEquals($mostPopular->id, $articles->first()->id);
+        $this->assertCount(4, $articles);
+    }
+}
