078f3e531bd92d9681f13803dc16409e5c2e46e7
Rizart Dokollari <r.dokollari@gmail.com>
2016-05-20 04:18:30 +0000

    Use sqlite testing database for test.

--- series/phpunit-testing-in-laravel/database/migrations/2016_05_19_044721_create_articles_table.php
+++ series/phpunit-testing-in-laravel/database/migrations/2016_05_19_044721_create_articles_table.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 class CreateArticlesTable extends Migration
@@ -16,7 +16,7 @@ class CreateArticlesTable extends Migration
         Schema::create('articles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->integer('reads');
+            $table->integer('reads')->default(0);
             $table->timestamps();
         });
     }
