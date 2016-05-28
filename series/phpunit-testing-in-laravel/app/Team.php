bcaa61a67e7a73801df2ac17fb5cffeecea0190a
Rizart Dokollari <r.dokollari@gmail.com>
2016-05-28 06:54:58 +0000

    Asset a team can add one member, has a maximum size, and adds multiple members at once.

--- series/phpunit-testing-in-laravel/app/Team.php
+++ series/phpunit-testing-in-laravel/app/Team.php
@@ -2,24 +2,43 @@
 
 namespace App;
 
+use Exception;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 class Team extends Model
 {
     protected $fillable = ['name', 'size'];
 
-    public function add(User $user)
+    public function add($users)
     {
-        $this->members()->save($user);
+        $this->guardAgainstTooManyMembers($users);
+
+        if ($users instanceof User) {
+            return $this->members()->save($users);
+        }
+
+        return $this->members()->saveMany($users);
     }
 
-    public function members()
+    private function guardAgainstTooManyMembers($newMembers)
     {
-        return $this->hasMany(User::class);
+        if ($this->count() >= $this->size) {
+            throw new Exception("Team cannot hold any more members.");
+        }
+
+        if ($this->count() + $newMembers->count() > $this->size) {
+            throw new Exception("Team maximum size is exceeded.");
+        }
     }
 
     public function count()
     {
         return $this->members()->count();
+    }
+
+    public function members()
+    {
+        return $this->hasMany(User::class);
     }
 }
