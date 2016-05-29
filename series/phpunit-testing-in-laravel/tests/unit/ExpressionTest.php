0dd7de2da09cb85411f9063091cdbe839ae45596
Rizart Dokollari <r.dokollari@gmail.com>
2016-05-29 16:55:53 +0000

    Create basic expression tests.

--- series/phpunit-testing-in-laravel/tests/unit/ExpressionTest.php
+++ series/phpunit-testing-in-laravel/tests/unit/ExpressionTest.php
@@ -7,11 +7,38 @@
 namespace tests\unit;
 
 
+use App\Expression;
+
 class ExpressionTest extends \PHPUnit_Framework_TestCase
 {
     /** @test */
     public function finds_a_string()
     {
-        
+        $regex = Expression::make()->find('www');
+        $this->assertRegExp($regex, 'www');
+
+        $regex = Expression::make()->then('www');
+        $this->assertRegExp($regex, 'www');
+    }
+
+    /** @test */
+    public function checks_for_anything()
+    {
+        $regex = Expression::make()->anything();
+
+        $this->assertRegExp($regex, 'foo');
+    }
+
+    /** @test */
+    public function maybe_has_a_value()
+    {
+        $regex = Expression::make()->maybe('http');
+        $this->assertRegExp($regex, 'http');
+        $this->assertRegExp($regex, '');
+    }
+
+    /** @test */
+    public function chains_method_calls()
+    {
     }
 }
