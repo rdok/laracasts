e881829da69c9d991ed854d23a54f33db3a3a303
Rizart Dokollari <r.dokollari@gmail.com>
2016-06-01 04:27:14 +0000

    Add examples of making API github & twitter calls using guzzle.

--- lessons/http-requests-with-guzzle/app/Http/routes.php
+++ lessons/http-requests-with-guzzle/app/Http/routes.php
@@ -1,16 +1,41 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Subscriber\Oauth\Oauth1;
 
-Route::get('/', function () {
-    return view('welcome');
+$this->get('github/users/{username}', function ($username) {
+    $client = new Client(['base_uri' => 'https://api.github.com/']);
+
+    $response = $client->get("users/$username");
+
+    $responseJson = json_decode($response->getBody(), true);
+
+    return $responseJson;
+});
+
+$this->get('twitter/search/{q}', function ($q) {
+    $stack = HandlerStack::create();
+
+    $middleware = new Oauth1([
+        'consumer_key'    => env('TWITTER_CONSUMER_KEY'),
+        'consumer_secret' => env('TWITTER_CONSUMER_SECRET'),
+        'token'           => env('TWITTER_TOKEN'),
+        'token_secret'    => env('TWITTER_TOKEN_SECRET'),
+    ]);
+    $stack->push($middleware);
+
+    $client = new Client([
+        'base_uri' => 'https://api.twitter.com/1.1/',
+        'handler'  => $stack,
+        'auth'     => 'oauth'
+    ]);
+
+    $response = $client->get("search/tweets.json?q=$q");
+
+    $responseJson = json_decode($response->getBody(), true);
+
+    $statuses =  array_only($responseJson, 'statuses');
+
+    return $statuses;
 });
