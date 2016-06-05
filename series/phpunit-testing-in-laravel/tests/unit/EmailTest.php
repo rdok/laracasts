ef1fff285a1ade2da6373c8bc57130852730f488
Rizart Dokollari <r.dokollari@gmail.com>
2016-06-05 07:25:55 +0000

    Initalize testing email with custom assertions.

---
+++ series/phpunit-testing-in-laravel/tests/unit/EmailTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Rizart Dokollari <r.dokollari@gmail.com>
+ * @since 6/5/16
+ */
+
+namespace tests\unit;
+
+use Mail;
+use Swift_Events_EventListener;
+use Swift_Events_SendEvent;
+
+class EmailTest extends \TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        Mail::getSwiftMailer()
+            ->registerPlugin(new TestingMailEventListener);
+    }
+
+    /** @test */
+    public function sent_email()
+    {
+        Mail::raw('Hello World', function ($message) {
+            $message->to('foo@bar.com');
+            $message->from('bar@foo.com');
+        });
+
+        $this->seeEmailWasSent();
+    }
+
+    public function seeEmailWasSent()
+    {
+    }
+}
+
+class TestingMailEventListener implements Swift_Events_EventListener
+{
+    public function beforeSendPerformed(Swift_Events_SendEvent $event)
+    {
+        $message = $event->getMessage();
+
+        dd($message->getFrom());
+    }
+}
