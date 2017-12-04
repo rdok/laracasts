3ae3892b8d9a3454dce01b83fda2a258d12111d0
Rizart Dokollari <r.dokollari@gmail.com>
2017-12-04 04:04:50 +0000

    Initiliaze tdd-forum.

---
+++ series/lets-build-a-forum-with-laravel/database/migrations/2017_12_04_005929_create_threads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateThreadsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('threads', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')
+                ->onDelete('restrict')->onUpdate('cascade');
+
+            $table->timestamps();
+
+            $table->string('title');
+            $table->text('body');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('threads', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('threads');
+    }
+}
